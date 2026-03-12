576dc1ef47c7e0e6df671a36ae18659b3b40bfdb
cho-sr <cho020218@gmail.com>
2026-03-12 21:03:41 +0900

    26.03.12

---
+++ add_minus.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int A[1001] = {23,40,13,48,20,8,45,3};
+    int B = 0;
+
+    printf("%d + %d = %d\n",A[0],A[1],A[0]+A[1]);
+    printf("%d - %d = %d\n",A[2],A[3],A[2]-A[3]);
+    printf("%d * %d = %d\n",A[4],A[5],A[4]*A[5]);
+    printf("%d / %d = %d\n",A[6],A[7],A[6]/A[7]);
+    return 0;
+
+            
+
+}
